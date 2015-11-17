eb8291c5d0d5d07a9afcac57d7a8769d62c40281
Bannanna <muchatorta@gmail.com>
2015-11-17 02:27:46 -0700

    added unittest file, added private config files

--- Py/main.py
+++ Py/main.py
@@ -1,6 +1,7 @@
-##
-import quandl.Q as Q
-API_KEY_MMAAMMBBUU = ''
-q = Q(API_KEY_MMAAMMBBUU) ##apikey is required ##QUANDL_BASE_API is the default URL
+##fix file structure it is abomination
+from lib.quandl.quandl import Q
+from config import configprivate as cfgp
+q = Q(cfgp.APIKEY) ##apikey is required ##QUANDL_BASE_API is the default URL
 ##methods: Q.quicky() Q.testRoute() Q.fullquery()
 ##attributes: Q.APIKEY Q.BASEURL Q.RATELIMITS
+print q.APIKEY
---
+++ Py/test.py
@@ -0,0 +1,6 @@
+import unittest
+
+from lib.quandl.quandl import Q
+from config import configprivate as cfgp
+
+q = Q(cfgp.APIKEY)
