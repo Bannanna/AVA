391dc1baff380b7c54d997871f6a69acc4445ba5
Bannanna <muchatorta@gmail.com>
2015-11-17 01:49:39 -0700

    removed api key

--- Py/main.py
+++ Py/main.py
@@ -1,6 +1,6 @@
 ##
 import quandl.Q as Q
-API_KEY_MMAAMMBBUU = '9mZBD-3sfS67X7xVA_6a'
+API_KEY_MMAAMMBBUU = ''
 q = Q(API_KEY_MMAAMMBBUU) ##apikey is required ##QUANDL_BASE_API is the default URL
 ##methods: Q.quicky() Q.testRoute() Q.fullquery()
 ##attributes: Q.APIKEY Q.BASEURL Q.RATELIMITS
